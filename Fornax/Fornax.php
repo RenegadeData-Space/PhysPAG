1cbf09be902710c89c92a44a8de36ab6f2007f65
Renegade Data <renegade@renegadedata.space>
2025-02-08 06:42:38 +0000

    added Fornax

---
+++ Fornax/Fornax.php
@@ -0,0 +1,387 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+<title>The Fornax Initiative</title>
+<!-- Meta -->
+	<meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="name" content="The Fornax Initiative">
+    <meta name="description" content="Web Site for The Fornax Initiative">
+    <meta name="keywords" content="Physics Astrophysics Cosmos NASA High-Energy X-ray Gamma-Ray Gamma Cosmic Gravity Gravitational Wave IPSAG IPSIG GRSAG GRSIG GWSAG SWSIG CRSAG CRSIG XRSAG XRSIG COSSIG PhysPAG Blackhole Neutron Star Pulsar Beyond Einstein BeyondEinstein">
+	<meta name="robots" content="noodp">
+    <meta name="doc-class" content="Living Document" />
+
+<!-- NASA Required Meta -->
+    <meta name="orgcode" content="660">
+    <meta name="rno" content="Brian Humensky">
+    <meta name="content-owner" content="Brian Humensky">
+    <meta name="webmaster" content="Stephan.F.Fantasia.1">
+
+<!-- Mobile Specific Meta --><!-- Pat Added second two lines -->
+<!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">-->
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
+
+<!-- Favicon -->
+    <link rel="shortcut icon" href="/modern/inc/img/favicon.png">
+
+<!-- CSS -->
+    <link rel="stylesheet" href="/modern/inc/css/news-scroll.css">
+    <link rel="stylesheet" href="/modern/inc/css/details.css">
+	<link rel="stylesheet" href="/modern/inc/css/cosmic.css">
+	<link rel="stylesheet" href="/modern/inc/css/default4.css">
+    <link rel="stylesheet" href="/modern/inc/css/probes-cards.css">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA&subagency=GSFC" id="_fed_an_ua_tag">
+</script>
+
+
+<!-- Javascript -->
+	<!-- Load html5shiv for old IE browsers -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!-- Google Analytics -->
+	<!--#include virtual="/jdm_modern/inc/html/analytics.html"-->
+
+</head>
+
+
+<body>
+<div id="skip"><a href="#content">Skip navigation and go straight to page content.</a></div> <!-- Skip To Content -->
+
+<header> <!-- Start NASA Header -->
+<div id="header-container"> <!-- Start Header Container -->
+<!-- NASA Logo -->
+	<div class="nasa-logo"><a href="https://www.nasa.gov"><img src="/modern/inc/img/NASA_logo.png" alt="NASA Logo"></a></div>
+<!-- Navigation (Hidden on Mobile/Tablet)-->
+	<div id="topnav">
+    <nav> <!-- Start PhysCOS Site Navigation -->
+<ul>
+	<li><a href="/">Home</a>
+	<ul>
+        <li><a href="/program/program-office.php">Program Office</a></li>
+		<li><a href="/science/science.php">Science</a></li>
+		<li><a href="/projects/current.php">Projects</a></li>
+		<li><a href="https://apd440.gsfc.nasa.gov/team.html" target="_blank">Project Office</a></li>
+	</ul>
+    </li>
+    
+	<li><a href="/physpag/physpag.php">PhysPAG</a>
+	<ul>
+		<li><a href="/physpag/ec/physpag-ec.php">Executive Committee</a></li>
+		<li><a href="/physpag/LISA/lisa-science-gaps.php">LISA Science Gaps</a></li>
+		<li><a href="/physpag/science-gaps/science-gaps.php">Science Gaps</a></li>
+		<li><a href="/physpag/program/cosmic-pathfinders.php">Cosmic Pathfinders</a></li>
+		<li><a href="/physpag/meetings.php">Meetings</a></li>
+		<li><a href="/physpag/events-calendar.php">Events Calendar</a></li>
+	</ul>
+    </li>
+    
+    <li><a href="/sigs/sigs.php">Science Interest Groups</a>
+    <ul>
+        <li><a href="/sigs/crnsig.php">Cosmic Ray &amp; Neutrino SIG</a></li>
+        <li><a href="/sigs/cossig.php">Cosmic Structure SIG</a></li>
+        <li><a href="/sigs/grsig.php">Gamma Ray SIG</a></li>
+        <li><a href="/sigs/gwsig.php">Gravitational Wave SIG</a></li>
+        <li><a href="/sigs/ipsig.php">Inflation Probe SIG</a></li>
+        <li><a href="/sigs/tdamm-sig.php">TDAMM SIG</a></li>
+        <li><a href="/sigs/xrsig.php">X-Ray SIG</a></li>
+
+    </ul>
+    </li>
+
+    <li><a href="/sags/sags.php">Science Analysis Groups</a>
+    <ul>
+        <li><a href="/sags/figsag.php">Future Innovations in Gamma Rays SAG</a></li>
+        <li><a href="/sags/ngo-sag.php">New Great Observatories SAG</a></li>
+        <li><a href="/sags/tdammcomm-sag.php">TDAMM Communications SAG</a></li>
+        <li><a href="/sags/Completed-SAGs.php">Completed SAGs</a></li>
+    </ul>
+    </li>
+
+	<li><a href="/studies/studies.php">Mission Studies</a>
+    <ul>
+				<li><a href="/Fornax/Fornax.php">The Fornax Initiative</a></li>
+		<li><a href="/multimessenger/multimessenger.php">PhysCOS TDAMM Initiative</a></li>
+        <li><a href="/missions/HEASwF/HEASwF_Working_Group.php">HEASwF Working Group</a></li>
+		<li><a href="/studies/L3/L3-study.php">NASA L3 Study (2016 &#8211; 2018)</a></li>
+		<li><a href="/studies/large-missions/Large-Missions-2020.php">Large Missions for Decadal 2020 (2015)</a></li>
+		<li><a href="/studies/athena/athena.php">NASA-ESA Athena Study (2014 &#8211; 2017)</a></li>
+		<li><a href="/studies/xray-probe/x-ray-probe-2013-2014.php">X-ray Astronomy Probe Study (2013 &#8211; 2014) </a></li>
+		<li><a href="/studies/architecting/architecting-studies-2012.php">2012 Architecting Studies</a></li>
+			</ul>
+    </li>
+
+	<li><a href="">Resources</a>
+    <ul>
+	    <li><a href="https://apd440.gsfc.nasa.gov/technology.html" target="_blank">PhysCOS Technology</a></li>
+		<li><a href="https://heasarc.gsfc.nasa.gov/" target="_blank">HEASARC Data Archive</a></li>
+		<li><a href="https://nspires.nasaprs.com/external/" target="_blank">NSPIRES</a></li>
+		<li><a href="/resources/Research_Opportunities.php">Research Opportunities</a></li>
+        <li><a href="/resources/habitable-worlds/hwo-events.php">HWO Seminar Series</a></li>
+        <li><a href="/science/career.php">NASA Career Opportunities</a></li>
+        <li><a href="/docs/documents.php">Documents</a>
+	</ul>
+    </li>
+
+
+</ul>
+</nav> <!-- End Site Navigation -->
+	</div>
+<!-- Mobile/Tablet Menu Access (Hidden on Desktop)-->
+	<div id="mobile-nav-icon">
+		<label for="menu-toggle" class="label-toggle"><img src="/modern/inc/img/hamburger_menu_icon.png" alt="Menu"></label>
+		<input id="menu-toggle" type="checkbox">
+    <nav> <!-- Start PhysCOS Site Navigation -->
+<ul>
+	<li><a href="/">Home</a>
+	<ul>
+        <li><a href="/program/program-office.php">Program Office</a></li>
+		<li><a href="/science/science.php">Science</a></li>
+		<li><a href="/projects/current.php">Projects</a></li>
+		<li><a href="https://apd440.gsfc.nasa.gov/team.html" target="_blank">Project Office</a></li>
+	</ul>
+    </li>
+    
+	<li><a href="/physpag/physpag.php">PhysPAG</a>
+	<ul>
+		<li><a href="/physpag/ec/physpag-ec.php">Executive Committee</a></li>
+		<li><a href="/physpag/LISA/lisa-science-gaps.php">LISA Science Gaps</a></li>
+		<li><a href="/physpag/science-gaps/science-gaps.php">Science Gaps</a></li>
+		<li><a href="/physpag/program/cosmic-pathfinders.php">Cosmic Pathfinders</a></li>
+		<li><a href="/physpag/meetings.php">Meetings</a></li>
+		<li><a href="/physpag/events-calendar.php">Events Calendar</a></li>
+	</ul>
+    </li>
+    
+    <li><a href="/sigs/sigs.php">Science Interest Groups</a>
+    <ul>
+        <li><a href="/sigs/crnsig.php">Cosmic Ray &amp; Neutrino SIG</a></li>
+        <li><a href="/sigs/cossig.php">Cosmic Structure SIG</a></li>
+        <li><a href="/sigs/grsig.php">Gamma Ray SIG</a></li>
+        <li><a href="/sigs/gwsig.php">Gravitational Wave SIG</a></li>
+        <li><a href="/sigs/ipsig.php">Inflation Probe SIG</a></li>
+        <li><a href="/sigs/tdamm-sig.php">TDAMM SIG</a></li>
+        <li><a href="/sigs/xrsig.php">X-Ray SIG</a></li>
+
+    </ul>
+    </li>
+
+    <li><a href="/sags/sags.php">Science Analysis Groups</a>
+    <ul>
+        <li><a href="/sags/figsag.php">Future Innovations in Gamma Rays SAG</a></li>
+        <li><a href="/sags/ngo-sag.php">New Great Observatories SAG</a></li>
+        <li><a href="/sags/tdammcomm-sag.php">TDAMM Communications SAG</a></li>
+        <li><a href="/sags/Completed-SAGs.php">Completed SAGs</a></li>
+    </ul>
+    </li>
+
+	<li><a href="/studies/studies.php">Mission Studies</a>
+    <ul>
+				<li><a href="/Fornax/Fornax.php">The Fornax Initiative</a></li>
+		<li><a href="/multimessenger/multimessenger.php">PhysCOS TDAMM Initiative</a></li>
+        <li><a href="/missions/HEASwF/HEASwF_Working_Group.php">HEASwF Working Group</a></li>
+		<li><a href="/studies/L3/L3-study.php">NASA L3 Study (2016 &#8211; 2018)</a></li>
+		<li><a href="/studies/large-missions/Large-Missions-2020.php">Large Missions for Decadal 2020 (2015)</a></li>
+		<li><a href="/studies/athena/athena.php">NASA-ESA Athena Study (2014 &#8211; 2017)</a></li>
+		<li><a href="/studies/xray-probe/x-ray-probe-2013-2014.php">X-ray Astronomy Probe Study (2013 &#8211; 2014) </a></li>
+		<li><a href="/studies/architecting/architecting-studies-2012.php">2012 Architecting Studies</a></li>
+			</ul>
+    </li>
+
+	<li><a href="">Resources</a>
+    <ul>
+	    <li><a href="https://apd440.gsfc.nasa.gov/technology.html" target="_blank">PhysCOS Technology</a></li>
+		<li><a href="https://heasarc.gsfc.nasa.gov/" target="_blank">HEASARC Data Archive</a></li>
+		<li><a href="https://nspires.nasaprs.com/external/" target="_blank">NSPIRES</a></li>
+		<li><a href="/resources/Research_Opportunities.php">Research Opportunities</a></li>
+        <li><a href="/resources/habitable-worlds/hwo-events.php">HWO Seminar Series</a></li>
+        <li><a href="/science/career.php">NASA Career Opportunities</a></li>
+        <li><a href="/docs/documents.php">Documents</a>
+	</ul>
+    </li>
+
+
+</ul>
+</nav> <!-- End Site Navigation -->
+	</div>
+
+</div> <!-- End Header Container -->
+</header> <!-- End NASA Header -->
+
+<div id="site-banner"> <!-- Start Site Banner -->
+	<div id="site-title">The Fornax Initiative</div>
+	<div id="site-subtitle">A NASA Astrophysics Science Platform</div>
+</div><!-- End Site Banner -->
+
+
+
+<div id="site-container"> <!-- Start Site Container -->
+
+<div id="page-wrapper"> <!-- Start Page Wrapper -->
+
+
+
+<div id="left-column">
+
+
+<hr>
+
+
+<ul>
+    <li>Fornax Presentation at AAS 245, Session 347, January 2025 [<a href="/Fornax/presentations/347.04 Sparke Open Science in the Cloud.pdf" target="_blank">PDF</a>]</li>
+    <li>The Fornax Initiative: Cloud Computing with Astrophysics Data Jan. 2025 [<a href="/Fornax/presentations/Jaffe_Fornax_PhysCOS.pdf" target="_blank">PDF</a>]</li>
+</ul>
+
+<hr>
+
+
+<p style="margin-top: 2rem;">NASA's Astrophysics missions generate vast and complex datasets offering immense scientific potential. The vital role of NASA's Astrophysics archives in mission success is evident, as they have demonstrated that user-focused, technologically-rich data systems are key science multipliers. As the Astronomy 2020 Decadal Review emphasized, in the coming decade, frontier science will be done with multi-wavelength and multi-messenger analysis across large, complex data sets, which will only increase the challenges of accessing big data, maintaining software, and obtaining sufficient computing resources.</p>
+
+<p><strong>NASA Astrophysics is developing the Fornax Initiative, a cloud-based system that brings together data, software, and computing so that researchers can focus on science.</strong></p>
+
+<h3 class="spaced-bottom">The three pillars of the Fornax Initiative project are:</h3>
+
+<div class="indent-box">
+
+<p class="indent-box"><strong>Fornax Scientific Components</strong>, the astrophysics-specific elements required to enable science in the cloud, including Python notebooks that demonstrate access to cloud-hosted NASA mission data, curated astrophysics software environments, and cloud-native services to support common astronomy workflows.</p>
+
+<p><strong>Fornax Science Console</strong>, a web-based application that users log into for access to cloud computing, data storage, and interactive data analysis in JupyterLab.</p>
+
+<p><strong>Science Support Systems</strong>, a program of engagement with the astronomical community, including a Helpdesk and training opportunities.</p>
+</div>
+
+<div class="sigImage" style="float: left; padding: 30px 0px 10px 30px;"><a href="/fornax/images/Scientific_info.png"><img alt="Scientific Information Graphic" src="/fornax/images/Scientific_info.png"></a></div>
+
+<div class="clear"></div>
+
+<p>Our goal is to support the science needs of a wide range of users, from those who are learning Python and would simply benefit from having access to a maintained Python environment for common astrophysics software to those who wish to perform complicated analyses that require significant cloud computing. For use cases that require more computing than can be provided by NASA through Fornax, we will build the system such that scientists can use the Fornax Scientific Components and the Fornax Science Console with their own cloud computing resources.</p>
+
+<p><strong>The Fornax Initiative is a collaboration of the NASA Astrophysics Archives with GSFC’s Astrophysics Projects Division (APD):</strong></p>
+
+<div class="logo-container" style="margin-top: 1rem;">
+<div class="logo-card">
+<a href="https://irsa.ipac.caltech.edu/frontpage/" title="NASA/IPAC Infrared Science Archive (IRSA)" target="_blank"><img class="logo-card" src="/fornax/images/IRSA-Logo_v2.png" alt="NASA/IPAC Infrared Science Archive (IRSA) Logo" title="NASA/IPAC Infrared Science Archive (IRSA)"></a>
+<div class="logo-title"><a href="https://irsa.ipac.caltech.edu/frontpage/" title="NASA/IPAC Infrared Science Archive (IRSA)" target="_blank">NASA/IPAC Infrared Science Archive<br>
+(IRSA)</a></div>
+</div>
+
+<div class="logo-card">
+<a href="https://archive.stsci.edu/" alt="Mikulski Archive for Space Telescopes (MAST)" title="Mikulski Archive for Space Telescopes (MAST)" target="_blank"><img class="logo-card" src="/fornax/images/MAST-Logo_v2.png" alt="Mikulski Archive for Space Telescopes (MAST) Logo" title="Mikulski Archive for Space Telescopes (MAST)"></a>
+<div class="logo-title"><a href="https://archive.stsci.edu/" alt="Mikulski Archive for Space Telescopes (MAST)" title="Mikulski Archive for Space Telescopes (MAST)" target="_blank">Mikulski Archive for Space Telescopes<br>
+(MAST)</a><br></div>
+</div>
+
+<div class="logo-card">
+<a href="https://heasarc.gsfc.nasa.gov/" title="High Energy Astrophysics Science Archive Research Center (HEASARC)" target="_blank"><img class="logo-card" src="/fornax/images/HEASARC-Logo_v2.png" alt="High Energy Astrophysics Science Archive Research Center (HEASARC) Logo" title="High Energy Astrophysics Science Archive Research Center (HEASARC)"></a>
+<div class="logo-title"><a href="https://heasarc.gsfc.nasa.gov/" title="High Energy Astrophysics Science Archive Research Center (HEASARC)" target="_blank">High Energy Astrophysics Science Archive Research Center (HEASARC)</a></div>
+</div>
+</div>
+
+
+
+</div>
+
+
+<div id="right-column">
+
+
+
+
+<div class="panel">
+	<div class="panel-title">News</div>
+	    <div class="panel-content">
+
+		<!-- Panel Item Entry -->
+		<div class="pi-date">4 February 2025</div>
+		<div class="pi-title">Habitable Worlds Observatory (HWO) Seminar Series</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/11_HWO_Seminar_Series.php">Details</a></div>
+
+		<!-- Panel Item Entry -->
+				<div class="pi-title">Removing DEIA Requirements from ROSES-2024</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/12_Removing_DEIA_Requirements_from_ROSES-2024.php">Details</a></div>
+
+		<!-- Panel Item Entry -->
+				<div class="pi-title">ROSES-24: Due Date Delays for Several Astrophysics Programs</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/13_ROSES-24_Due_Date_Delays.php">Details</a></div>
+
+		<!-- Panel Item Entry -->
+				<div class="pi-title">ROSES-24 F.5 FINESST: SMD’s Graduate Student Research Clarifications and Corrections, and Reviewers Sought</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/14_ROSES-24_F.5-FINESST_Clarifications.php">Details</a></div>
+
+		<!-- Panel Item Entry -->
+				<div class="pi-title">ROSES-24: D.13 Astrophysics Pioneers Substantial Revisions</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/15_Draft_AO_Astrophysics_Small_Explorers.php">Details</a></div>
+
+		<!-- Panel Item Entry -->
+				<div class="pi-title">ROSES-24: E.12 Physical Sciences Research Studies Final Text and Due Dates</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/16_ROSES-24_E.12_Physical_Sciences_Research.php">Details</a></div>
+
+<div class="flexMenu">
+    <div class="MenuItem readRight"><a href="/news/news-archive.php">News Archive</a></div>
+</div>
+
+<hr>
+
+<div class="flexMenu">
+    <div class="MenuItem readRight"><a href="/physcos-news-email-list.php">Subscribe to PhysCOS News</a></div>
+</div>
+
+    </div></div>
+
+</div>
+
+
+
+
+</div> <!-- End Page Wrapper -->
+<hr>
+</div> <!-- End Site Container -->
+
+<!-- Start Footer -->
+<footer>
+<div id="footer-container"> <!-- Start Footer Container -->
+<!-- NASA Logo -->
+	<div class="nasa-logo">
+	<a href="https://www.nasa.gov"><img src="/modern/inc/img/NASA_logo.png" width="60" alt="NASA Logo"></a>
+	</div>
+<!-- Contacts -->
+	<div id="contacts">
+	<ul>
+		<li>Curator: <a href="&#109;&#097;&#105;&#108;&#116;&#111;:patricia.a.tyler@nasa.gov">Pat Tyler</a></li>
+		<li>NASA Official: <a href="mailto:thomas.b.humensky@nasa.gov">Brian Humensky</a></li>
+	</ul>
+	</div>
+<!-- Notices -->
+	<div id="notices">
+	<ul>
+				<li><a href="https://www.nasa.gov/privacy/">Privacy Policy and Important Notices</a></li>
+		<li><a href="https://www.nasa.gov/general/nasa-contact-page/">Contact NASA</a></li>
+<!--#config timefmt="%a, %b %d, %Y"--><!--#echo var="LAST_MODIFIED" -->	</ul>
+	</div>
+</div> <!-- End Footer Container -->
+</footer>
+<!-- End Footer -->
+
+</body>
+</html>
